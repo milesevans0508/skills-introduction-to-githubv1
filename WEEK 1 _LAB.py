184d78c670c6c795b441a896cf714ba07834b6a1
milesevans0508 <miles.evans0508@gmail.com>
2025-10-07 15:07:39 +0100

    Add files via upload

---
+++ WEEK 1 _LAB.py
@@ -0,0 +1,10 @@
+print("Hello World")
+student_name = "Alex"
+student_age = 20
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+num1 = 15
+num2 = 10
+total = num1 + num2
+print("The sum is:", total)
